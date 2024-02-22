5c776ca0830fe87d3c5c08bb28d887d28ca4c76e
jameshsu1125 <jameshsu1125@gmail.com>
2024-02-22 15:47:40 +0800

    Add console.log statement to log HTML value

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -15,6 +15,7 @@ const News = memo(() => {
   const [html] = useSelect({ collection: SETTING.mongodb[1].collection });
   const [insertRespond, getInsert] = useInsert();
   const [updateRespond, getUpdate] = useUpdate();
+  console.log(html);
 
   useEffect(() => {
     if (insertRespond?.res) {
